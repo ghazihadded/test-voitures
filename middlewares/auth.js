e2f4f49ee2f3bad1714d64c77f0e176aa615e73f
ghazi hadded <ghazihadded6@gmail.com>
2021-07-18 21:06:28 +0200

    auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,7 +13,7 @@ const auth = async (req, res, next) => {
     req.user = coded.user;
     next();
   } catch (err) {
-    rers.status(500).send([{ msg: "token not valid" }]);
+    res.status(500).send([{ msg: "token not valid" }]);
   }
 };
 
